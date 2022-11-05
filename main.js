3091609b936175e2568ec1a109ffe3a1eb422ca0
samaragr24 <116603622+samaragr24@users.noreply.github.com>
2022-11-04 18:35:23 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function playNote(key) {
+    // Obtener la nota
+    const noteAudio = document.getElementById(key.dataset.note);
+    // Resetear la ejecución de la nota
+    noteAudio.currentTime = 0;
+    // Utilizar método play() de la nota
+    noteAudio.play();
+  
+    // Añadimos clase active
+    key.classList.add('active');
+    // Agregamos un event listener al estado ended del evento keydown
+    noteAudio.addEventListener('ended', () => {
+      // Cuando termine de reproducirse el sonido, removemos clase active
+      key.classList.remove('active');
+    });
+  }
+  
+  const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+  const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+  
+  const keys = document.querySelectorAll('.key');
+  const whiteKeys = document.querySelectorAll('.key.white');
+  const blackKeys = document.querySelectorAll('.key.black');
+  
+  keys.forEach((key) => {
+    key.addEventListener('click', () => playNote(key));
+  });
+  
+  document.addEventListener('keydown', (e) => {
+    if (e.repeat) return;
+    const key = e.key;
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blackKeyIndex = BLACK_KEYS.indexOf(key);
+    console.log(whiteKeys[whiteKeyIndex]);
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
+  });
